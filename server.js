50d0cadae5dc9f27c6ee9be247d3c44b76649e16
Demur Rumed <gunkmute@gmail.com>
2015-05-25 14:36:16 +0000

    More unwritten modules

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ process.chdir(__dirname);
 var db = require("redis").createClient();
 var app = require("connect")().
 	use(require("compression")()).
-	use(require("serve-static")(__dirname, { maxAge: 2626262000 })).
-	use("/setdeck", require("./setdeck")(db)).
-	use("/getdecks", require("./getdecks")(db)).
-	use("/standings", require("./standings")(db));
+	use(require("serve-static")(__dirname, { maxAge: 2626262000 }));
+// TODO WM interfaces for setting match results, match generator, assigning penalty, dead line logic
+["setdeck", "getdecks", "setdiscard", "setsalvage", "viewvault", "standings"].forEach(function(mod){
+	app.use("/"+mod, require("./"+mod)(db));
+});
+app.listen(80);
