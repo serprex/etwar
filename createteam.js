2a83f69a623391b911ff27f28a6e83a7e45da411
Demur Rumed <gunkmute@gmail.com>
2015-05-28 19:53:22 +0000

    Gracefully handle creating team multiple times, start on player's interface

--- createteam.js
+++ createteam.js
@@ -3,8 +3,10 @@ module.exports = function(db){
 	return function(req, res, next){
 		// TODO verify opt.auth
 		var opt = qs.parse(req.url.slice(2)), ele = opt.e;
-		db.hmset("E"+ele+":VAULT", {elec: opt.s, ele: ele, cards: "", salvage: "", discard: "", salvagepool: "", discardpool: "", salvagecount: 0, discardcount: 0, relics: 0, psw: "nopass"});
-		db.lpush("E"+ele+":1:DECKS", "", "", "", "", "", "");
-		db.lpush("E"+ele+":1:SIDES", "", "", "", "", "", "");
+		db.del("E"+ele+":VAULT", "E"+ele+":1:DECKS", "E"+ele+":1:SIDES", function(){
+			db.hmset("E"+ele+":VAULT", {elec: opt.s, ele: ele, cards: "", salvage: "", discard: "", salvagepool: "", discardpool: "", salvagecount: 0, discardcount: 0, relics: 0, psw: "nopass"});
+			db.lpush("E"+ele+":1:DECKS", "", "", "", "", "", "");
+			db.lpush("E"+ele+":1:SIDES", "", "", "", "", "", "");
+		});
 	}
 }
