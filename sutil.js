690a6e2df60d0b31fcef79f436594e03154ec4e8
Demur Rumed <gunkmute@gmail.com>
2015-05-29 22:57:46 +0000

    s/opts/opt/

--- sutil.js
+++ sutil.js
@@ -1,20 +1,20 @@
 var qs = require("querystring");
-exports.parseOpts = function(req){
+exports.parseOpt = function(req){
 	return qs.parse(req.url.slice(2));
 }
 exports.verifyAuth = function(db, req, cb){
-	var opt = exports.parseOpts(req), e = opts.e;
+	var opt = exports.parseOpt(req), e = opt.e;
 	db.hget("E"+e+":VAULT", "psw", function(err, psw){
 		if (!err && psw == opt.auth){
-			cb(opts);
+			cb(opt);
 		}
 	});
 }
 exports.verifyWmAuth = function(db, req, cb){
-	var opts = exports.parseOpts(req);
+	var opt = exports.parseOpt(req);
 	db.get("wmauth", function(err, psw){
-		if (!err && psw == opts.auth){
-			cb(opts);
+		if (!err && psw == opt.auth){
+			cb(opt);
 		}
 	});
 }
--- viewvault.js
+++ viewvault.js
@@ -1,8 +1,8 @@
 var sutil = require("./sutil");
 module.exports = function(db){
 	return function(req, res, next){
-		sutil.verifyAuth(db, req, function(opts){
-			db.hgetall("E"+opts.e+":VAULT", function(err, vault){
+		sutil.verifyAuth(db, req, function(opt){
+			db.hgetall("E"+opt.e+":VAULT", function(err, vault){
 				if (vault){
 					delete vault.psw;
 					res.writeHead(200, {"Content-Type": "text/json"});
