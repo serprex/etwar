64ccd81c03771c47cd54e6f0d471c299f7616df0
Demur Rumed <gunkmute@gmail.com>
2015-05-31 19:04:38 +0000

    Basic setvault

--- setvault.js
+++ setvault.js
@@ -1,4 +1,14 @@
+var sutil = require("./sutil");
 module.exports = function(db){
 	return function(req, res, next){
+		sutil.verifyAuth(db, req, function(opt){
+			db.hgetall("E"+opt.e+":VAULT", function(err, vault){
+				var cards = opt.cards.split(" ");
+				// TODO correct verify logic
+				if (cards.length <= vault.elec){
+					db.hset("E"+opt.e+":VAULT", "cards", opt.cards);
+				}
+			});
+		});
 	}
 }
