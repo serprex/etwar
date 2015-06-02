2413b7c849fa3e86fd39e11a5a1fbc0619711d73
Demur Rumed <gunkmute@gmail.com>
2015-06-02 01:40:54 +0000

    Use tier data to simulate pricing for now

--- setvault.js
+++ setvault.js
@@ -1,11 +1,17 @@
 var sutil = require("./sutil");
+var carddb = require("./carddb");
 module.exports = function(db){
 	return function(req, res, next){
 		sutil.verifyAuth(db, req, function(opt){
 			db.hgetall("E"+opt.e+":VAULT", function(err, vault){
-				var cards = opt.cards.split(" ");
-				// TODO correct verify logic
-				if (cards.length <= vault.elec){
+				var cards = opt.cards.split(" "), cost = 0;
+				cards.forEach(function(code){
+					var card = carddb[code];
+					if (card){
+						cost += card.tier;
+					}
+				});
+				if (cost <= vault.elec){
 					db.hset("E"+opt.e+":VAULT", "cards", opt.cards);
 				}
 			});
