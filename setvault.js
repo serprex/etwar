98b897a06150a5cd2cc4d00032d11b8ad249a868
Demur Rumed <gunkmute@gmail.com>
2015-06-17 15:02:11 +0000

    Update setvault with pricing, relic padding

--- setvault.js
+++ setvault.js
@@ -5,12 +5,18 @@ module.exports = sutil.verifyAuth(function(opt, req, res, db){
 		var cards = opt.cards.split(" "), cost = 0;
 		cards.forEach(function(code){
 			var card = carddb[code];
-			if (card){
-				cost += card.tier;
+			if (code < "6qo" && code != "4t9" && card && card.type){
+				cost += card.price;
+			}else{
+				cost += vault.elec;
 			}
 		});
+		while (opt.cards.length < 180) opt.cards.push("4t9");
 		if (cost <= vault.elec){
 			db.hset("E"+opt.e+":VAULT", "cards", opt.cards);
+		}else{
+			res.writeHead(403);
+			res.end();
 		}
 	});
 });
